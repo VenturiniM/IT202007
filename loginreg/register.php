bcd5abdc5e8d1637a58af1d2001710216b27abda
Matthew A Venturini <mv283@afsconnect1.njit.edu>
2020-09-28 18:30:15 -0400

    updated login

--- loginreg/register.php
+++ loginreg/register.php
@@ -1,4 +1,4 @@
-form method="POST">
+<form method="POST">
   <label for="email">Email:</label>
   <input type="email" id="email" name="email" required/>
   <label for="p1">Password:</label>
@@ -7,7 +7,6 @@ form method="POST">
   <input type="password" id="p2" name="confirm" required/>
   <input type="submit" name="register" value="Register"/>
 </form>
-
 <?php
 if(isset($_POST["register"])){
   $email = null;
@@ -25,9 +24,39 @@ if(isset($_POST["register"])){
   $isValid = true;
   //check if passwords match on the server side
   if($password == $confirm){
-    echo "Passwords match <br>";
+    echo "Passwords match <br>"; 
   }
   else{
     echo "Passwords don't match<br>";
     $isValid = false;
-
+  }
+  if(!isset($email) || !isset($password) || !isset($confirm)){
+   $isValid = false; 
+  }
+  //TODO other validation as desired, remember this is the last line of defense
+  if($isValid){
+    $hash = password_hash($password, PASSWORD_BCRYPT);
+    require_once("db.php");
+    $db = getDB();
+	if(isset($db)){
+		//here we'll use placeholders to let PDO map and sanitize our data
+		$stmt = $db->prepare("INSERT INTO Users(email, password) VALUES(:email, :password)");
+		//here's the data map for the parameter to data
+		$params = array(":email"=>$email, ":password"=>$hash);
+		$r = $stmt->execute($params);
+		//let's just see what's returned
+		echo "db returned: " . var_export($r, true);
+		$e = $stmt->errorInfo();
+		if($e[0] == "00000"){
+			echo "<br>Welcome! You successfully registered, please login.";
+		}
+		else{
+			echo "uh oh something went wrong: " . var_export($e, true);
+		}
+	}
+  }
+  else{
+   echo "There was a validation issue"; 
+  }
+}
+?>
