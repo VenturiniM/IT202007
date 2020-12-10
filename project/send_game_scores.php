da89ca189128fd9817f3d9ec09697c196b99b08c
Matthew A Venturini <mv283@afsconnect1.njit.edu>
2020-12-09 21:16:31 -0500

    Adding file to send scores to db

--- project/send_game_scores.php
+++ project/send_game_scores.php
@@ -1,9 +1,11 @@
-<?php require_once(__DIR__ . "/partials/helpers.php"); ?>
-<?php
-if (is_logged_in() && isset($_POST["score"])) {
-  //$userid = $_POST["user_id"];
+<?php 
+require_once(__DIR__ . "/lib/helpers.php"); 
+
+if (!is_logged_in()) {
+   die(header(':', true, 403));
+ }
    $userid = get_user_id();
-   $score = $_POST["score"];
+   $score = $_POST["sendscore"];
 
    $db = getDB();
    $stmt = $db->prepare("INSERT INTO Scores (user_id, score) VALUES(:userid, :score)");
@@ -11,17 +13,4 @@ if (is_logged_in() && isset($_POST["score"])) {
 	":userid"=>$userid,
 	":score"=>$score,
 	]);
-   
-   if ($r) {
-    $response = ["status" => 200, "score" => $score];
-    echo json_encode($response);
-    die();
-}
-else {
-    $e = $stmt->errorInfo();
-    $response = ["status" => 400, "error" => $e];
-    echo json_encode($response);
-    die();
-}
-}
 ?>
